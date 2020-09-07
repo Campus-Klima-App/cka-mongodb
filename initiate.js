1634ba69b685b65c0c5e17297978d46218ffa115
M.Schwarz <Michel_Schwarz@ymail.com>
2020-09-07 21:46:18 +0200

    fix: changed initiate function to reconfig()

--- initiate.js
+++ initiate.js
@@ -1,10 +1,7 @@
-rs.initiate(
-    {
-        _id : 'rs1',
-        members: [
-            { _id : 0, host : "cka-mongodb1:27017" },
-            { _id : 1, host : "cka-mongodb2:27017" },
-            { _id : 2, host : "cka-mongodb3:27017" }
-        ]
-    }
-)
+rsconf = rs.conf();
+rsconf.members = [
+    { _id : 0, host : "cka-mongodb1:27017" },
+    { _id : 1, host : "cka-mongodb2:27017" },
+    { _id : 2, host : "cka-mongodb3:27017" }
+];
+rs.reconfig(rsconf, {force: true});
