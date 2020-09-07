dc702005f53ee8923093d487c1c8fbd882dc8643
M.Schwarz <Michel_Schwarz@ymail.com>
2020-09-07 21:08:16 +0200

    fix: changed host name in initiate.js

--- initiate.js
+++ initiate.js
@@ -2,9 +2,9 @@ rs.initiate(
     {
         _id : 'rs1',
         members: [
-            { _id : 0, host : "mongo1:27017" },
-            { _id : 1, host : "mongo2:27017" },
-            { _id : 2, host : "mongo3:27017" }
+            { _id : 0, host : "cka-mongodb1:27017" },
+            { _id : 1, host : "cka-mongodb2:27017" },
+            { _id : 2, host : "cka-mongodb3:27017" }
         ]
     }
 )
